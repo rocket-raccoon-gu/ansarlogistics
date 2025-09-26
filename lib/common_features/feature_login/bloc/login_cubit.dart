969b644f7ade8bca82d1c51ed59fb0e14c43d56c
rocket-raccoon-gu <187929382+shahadansargallery@users.noreply.github.com>
2025-09-26 23:10:49 +0300

    commit

--- lib/common_features/feature_login/bloc/login_cubit.dart
+++ lib/common_features/feature_login/bloc/login_cubit.dart
@@ -109,7 +109,7 @@ class LoginCubit extends Cubit<LoginState> {
 
         final info = await PackageInfo.fromPlatform();
 
-        // final String serverkey = await getAccessToken();
+        final String serverkey = await getAccessToken();
 
         await PreferenceUtils.storeDataToShared("devicetoken", value);
 
@@ -158,7 +158,7 @@ class LoginCubit extends Cubit<LoginState> {
                 userId: userId,
                 password: password,
                 token: value,
-                bearertoken: "",
+                bearertoken: serverkey,
                 appversion: info.version,
               );
           DateTime responseTime = DateTime.now();
