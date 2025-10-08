dcab1c2a12f4ec43ca0147024f1d33cb99967680
rocket-raccoon-gu <187929382+shahadansargallery@users.noreply.github.com>
2025-10-08 14:19:35 +0300

    commit

--- picker_driver_api/lib/responses/order_report_response.dart
+++ picker_driver_api/lib/responses/order_report_response.dart
@@ -14,39 +14,37 @@ class OrderReportsResponse {
   int success;
   List<StatusHistory> statusHistories;
 
-  OrderReportsResponse({
-    required this.success,
-    required this.statusHistories,
-  });
+  OrderReportsResponse({required this.success, required this.statusHistories});
 
   OrderReportsResponse copyWith({
     int? success,
     List<StatusHistory>? statusHistories,
-  }) =>
-      OrderReportsResponse(
-        success: success ?? this.success,
-        statusHistories: statusHistories ?? this.statusHistories,
-      );
+  }) => OrderReportsResponse(
+    success: success ?? this.success,
+    statusHistories: statusHistories ?? this.statusHistories,
+  );
 
   factory OrderReportsResponse.fromJson(Map<String, dynamic> json) =>
       OrderReportsResponse(
         success: json["success"],
         statusHistories: List<StatusHistory>.from(
-            json["status_histories"].map((x) => StatusHistory.fromJson(x))),
+          json["status_histories"].map((x) => StatusHistory.fromJson(x)),
+        ),
       );
 
   Map<String, dynamic> toJson() => {
-        "success": success,
-        "status_histories":
-            List<dynamic>.from(statusHistories.map((x) => x.toJson())),
-      };
+    "success": success,
+    "status_histories": List<dynamic>.from(
+      statusHistories.map((x) => x.toJson()),
+    ),
+  };
 }
 
 class StatusHistory {
   dynamic orderCount;
   String status;
   DateTime? createdAt;
-  String? orderIds;
+  List<String>? orderIds;
 
   StatusHistory({
     required this.orderCount,
@@ -59,29 +57,32 @@ class StatusHistory {
     dynamic orderCount,
     String? status,
     DateTime? createdAt,
-    String? orderIds,
-  }) =>
-      StatusHistory(
-        orderCount: orderCount ?? this.orderCount,
-        status: status ?? this.status,
-        createdAt: createdAt ?? this.createdAt,
-        orderIds: orderIds ?? this.orderIds,
-      );
+    List<String>? orderIds,
+  }) => StatusHistory(
+    orderCount: orderCount ?? this.orderCount,
+    status: status ?? this.status,
+    createdAt: createdAt ?? this.createdAt,
+    orderIds: orderIds ?? this.orderIds,
+  );
 
   factory StatusHistory.fromJson(Map<String, dynamic> json) => StatusHistory(
-        orderCount: json["order_count"],
-        status: json["status"],
-        createdAt: json["created_at"] == null
+    orderCount: json["order_count"],
+    status: json["status"],
+    createdAt:
+        json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+    orderIds:
+        json["order_ids"] == null
             ? null
-            : DateTime.parse(json["created_at"]),
-        orderIds: json["order_ids"],
-      );
+            : (json["order_ids"] as String)
+                .split(',')
+                .map((id) => id.trim())
+                .toList(),
+  );
 
   Map<String, dynamic> toJson() => {
-        "order_count": orderCount,
-        "status": status,
-        "created_at":
-            "${createdAt!.year.toString().padLeft(4, '0')}-${createdAt!.month.toString().padLeft(2, '0')}-${createdAt!.day.toString().padLeft(2, '0')}",
-        "order_ids": orderIds,
-      };
+    "order_count": orderCount,
+    "status": status,
+    "created_at": createdAt?.toIso8601String(),
+    "order_ids": orderIds?.join(','),
+  };
 }
