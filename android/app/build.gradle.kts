95504de3065a64041fd3e6ecc8df37e627a252f7
rocket-raccoon-gu <136160713+rocket-raccoon-gu@users.noreply.github.com>
2026-07-05 12:14:39 +0300

    commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -70,7 +70,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
-    implementation("com.google.gms:google-services:4.3.15")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
